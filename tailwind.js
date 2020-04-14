1bc831f70c12ada5599e83f8b835955c225b2879
thegrinder <paul.szczepkowski@gmail.com>
2020-04-14 19:59:02 +0100

    Fix hot reloading by importing plugins directly in tailwind.js file

--- tailwind.js
+++ tailwind.js
@@ -1,4 +1,17 @@
-const plugins = require('.');
+// utilities
+const flexGrid = require('./src/utilities/flex-grid');
+const spacing = require('./src/utilities/spacing');
+const borderRadius = require('./src/utilities/border-radius');
+const border = require('./src/utilities/border');
+const custom = require('./src/utilities/custom');
+const display = require('./src/utilities/display');
+const fontWeight = require('./src/utilities/font-weight');
+const textAlign = require('./src/utilities/text-align');
+
+// components
+const button = require('./src/components/button/button');
+const text = require('./src/components/text/text');
+const heading = require('./src/components/heading/heading');
 
 module.exports = {
   theme: {
@@ -94,7 +107,19 @@ module.exports = {
     'width',
     'zIndex',
   ],
-  plugins,
+  plugins: [
+    borderRadius,
+    flexGrid,
+    spacing,
+    border,
+    custom,
+    display,
+    fontWeight,
+    textAlign,
+    button,
+    text,
+    heading,
+  ],
   options: {
     prefix: '',
     important: false,
