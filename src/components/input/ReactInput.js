18a241793cb6ecdcd08264a5df92d962fc5b2d11
thegrinder <paul.szczepkowski@gmail.com>
2020-05-02 09:02:14 +0100

    Add start and end icon to input

--- src/components/input/ReactInput.js
+++ src/components/input/ReactInput.js
@@ -6,22 +6,46 @@ const Input = ({
   valid = false,
   invalid = false,
   disabled = false,
+  startIcon,
+  endIcon,
   className,
   placeholder,
 }) => {
   const [colorMode] = useColorMode();
   return (
-    <input
-      disabled={disabled}
-      className={classNames({
-        input: true,
-        [className]: true,
-        [`${colorMode}:input`]: true,
-        [`${colorMode}:input-valid`]: valid,
-        [`${colorMode}:input-invalid`]: invalid,
-      })}
-      placeholder={placeholder}
-    />
+    <div className={classNames('relative', className)}>
+      {(startIcon || endIcon) && (
+        <span
+          className={classNames(
+            'absolute',
+            'inset-y-0',
+            'flex',
+            'items-center',
+            'justify-center',
+            'w-10',
+            `${colorMode}:text-neutral-600`,
+            {
+              'left-0': !!startIcon,
+              'right-0': !!endIcon,
+            }
+          )}
+        >
+          {startIcon || endIcon}
+        </span>
+      )}
+      <input
+        disabled={disabled}
+        className={classNames({
+          input: true,
+          'pl-8': !!startIcon,
+          'pr-8': !!endIcon,
+          [`${colorMode}:input`]: true,
+          [`${colorMode}:input-valid`]: valid,
+          [`${colorMode}:input-invalid`]: invalid,
+        })}
+        placeholder={placeholder}
+      />
+    </div>
   );
 };
 
